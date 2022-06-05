1f7f82b51fc298144e1ba0ec3e7eb5aa1b8f306e
Paulo Henrique <42096755+paulowh@users.noreply.github.com>
2022-06-05 02:12:56 -0300

    incremento de verificação das jogadas

--- main.py
+++ main.py
@@ -35,6 +35,56 @@ def criarTabuleiro(qtdLinha, qtdColuna ):
             cont += 1
     wb.save('banco.xlsx')
 
-wb = load_workbook(filename='banco.xlsx', read_only=True)
-jogo = wb['jogo1']
-print(jogo.cell(column=2, row=1).value)
+def lerJogo():
+    wb = load_workbook(filename='banco.xlsx', read_only=True)
+    jogo = wb['jogo1']
+    linha = jogo.max_row
+    coluna = jogo.max_column
+
+    for i in range(1, linha+1):
+        for j in range(1, coluna + 1):
+            print('{:^3}'.format(jogo.cell(column=j, row=i).value), end='|')
+            #print('{:^5}'.format('[ ]'), end='')
+        print('\n')
+
+def verificarJogada():
+    wb = load_workbook(filename='banco.xlsx', read_only=True)
+    jogo = wb['jogo1']
+    maxLinha = jogo.max_row
+    maxColuna = jogo.max_column
+    gameOver = False
+    jogadas = []
+
+    while True:
+        n1 = int(input('linha: '))
+        n2 = int(input('coluna: '))
+        #teste de jogadas
+        jogadas.append(jogo.cell(column=n2, row=n1))
+
+        for i in range(1, maxLinha+1):
+            for j in range(1, maxColuna + 1):
+                passouK = False
+                for k in jogadas:  
+                    #print(k, jogo.cell(column=j, row=i))
+                    if k == jogo.cell(column=j, row=i) and jogo.cell(column=j, row=i).value == 0:
+                        print('{:^5}'.format('[0]'), end='')
+                        passouK = True
+                    elif k == jogo.cell(column=j, row=i) and jogo.cell(column=j, row=i).value == -1:
+                        print('{:^5}'.format('[x]'), end='')
+                        passouK = True
+                        gameOver = True
+                if passouK == False:
+                    print('{:^5}'.format('[ ]'), end='')
+            print('\n')
+
+        if gameOver == True:
+            print('Game Over!!!')
+            break
+    
+verificarJogada()
+
+
+    
+
+
+
