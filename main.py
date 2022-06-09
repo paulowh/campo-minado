622097d5f8bb640167076f53b365e131961486be
Paulo <ph744915@gmail.com>
2022-06-09 16:32:37 -0300

    correção no modulo de geração de bombas

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
-from random import Random
+import random
 from openpyxl import Workbook, load_workbook
 
 def bombinhas(totalCasas):
-    random = Random()
-    dificuldade = 3 # 1 = facil | 2 = médio | 3 = dificil
-    lcBombas = 0
+    dificuldade = 3  # 1 = facil | 2 = médio | 3 = dificil
     bombas = []
     dificuldade, linhas, colunas = configuracoes()
 
-    #NIVEL DE DIFICULDADE
+    # NIVEL DE DIFICULDADE
     if dificuldade == 1:
-        qtdBombas = int(totalCasas*0.15) #int => para 'truncar o numero'
+        qtdBombas = int(totalCasas*0.15)  # int => para 'truncar o numero'
     elif dificuldade == 2:
         qtdBombas = int(totalCasas*0.25)
     else:
         qtdBombas = int(totalCasas*0.50)
 
-    #ALETORIEDADE DAS BOMBAS
-    for i in range(qtdBombas):
-        lcBombas = random.randrange(1, totalCasas)
-        if lcBombas in bombas:
-            lcBombas = random.randrange(1, totalCasas)
-        bombas.append(lcBombas)
+    # ALETORIEDADE DAS BOMBAS
+    while True:
+        num = random.randint(1, totalCasas)
+        if num not in bombas:
+            bombas.append(num)
+        if len(bombas) == qtdBombas:
+            break
 
     return bombas
 
 def criarTabuleiro(qtdLinha, qtdColuna):
     try:
-        wb = load_workbook(filename='banco.xlsx')
+        wb = load_workbook(filename='banco.xlsx', read_only=False)
     except:
         wb = Workbook()
-        wb.active.title = 'jogo'
-    
+
     try:
         abaJogo = wb['jogo']
     except:
@@ -42,7 +40,7 @@ def criarTabuleiro(qtdLinha, qtdColuna):
 
     bombas = bombinhas(qtdColuna*qtdLinha)
 
-    #CRIAÇÃO DO TABULEIRO
+    # CRIAÇÃO DO TABULEIRO
     for i in range(1, qtdLinha+1):
         for j in range(1, qtdColuna+1):
 
@@ -52,6 +50,7 @@ def criarTabuleiro(qtdLinha, qtdColuna):
                 abaJogo.cell(row=i, column=j, value=vazio)
             cont += 1
     wb.save('banco.xlsx')
+
 
 def verificarJogada():
     wb = load_workbook(filename='banco.xlsx', read_only=False)
@@ -65,21 +64,22 @@ def verificarJogada():
     while True:
         n1 = int(input('linha: '))
         n2 = int(input('coluna: '))
-        #teste de jogadas
-        
+        # teste de jogadas
+
         if jogo.cell(column=n2, row=n1) not in jogadas:
             jogadas.append(jogo.cell(column=n2, row=n1))
             for i in range(1, maxLinha+1):
                 for j in range(1, maxColuna + 1):
                     passouK = False
-                    for k in jogadas:  
-                        #print(k, jogo.cell(column=j, row=i))
+                    for k in jogadas:
+                        # print(k, jogo.cell(column=j, row=i))
                         if k == jogo.cell(column=j, row=i) and jogo.cell(column=j, row=i).value == 0:
-                            print('{:^5}'.format(jogo.cell(column=j, row=i).value), end='')
+                            print('{:^5}'.format(
+                                jogo.cell(column=j, row=i).value), end='')
                             passouK = True
                             if len(jogadas) == (qtdColuna*qtdLinha - len(bombinhas(qtdColuna*qtdLinha))):
                                 ganhou = True
-                                
+
                         elif k == jogo.cell(column=j, row=i) and jogo.cell(column=j, row=i).value == -1:
                             print('{:^5}'.format('X'), end='')
                             passouK = True
@@ -97,6 +97,7 @@ def verificarJogada():
             print('Você Ganhou')
             break
 
+
 def configuracoes():
     try:
         wb = load_workbook(filename='banco.xlsx')
@@ -110,13 +111,14 @@ def configuracoes():
         config.cell(column=2, row=2, value=3)
         config.cell(column=1, row=3, value='Dificuldade')
         config.cell(column=2, row=3, value=2)
-    
+
     dificuldade = config.cell(column=2, row=3).value
     linhas = config.cell(column=2, row=1).value
     colunas = config.cell(column=2, row=2).value
     wb.save('banco.xlsx')
-    
+
     return int(dificuldade), int(linhas), int(colunas)
+
 
 def alterarConfiguracao():
     try:
@@ -137,8 +139,9 @@ def alterarConfiguracao():
     config.cell(column=2, row=3, value=dificuldade)
     wb.save('banco.xlsx')
 
-#alterarConfiguracao()
+# alterarConfiguracao()
 
 dificuldade, linhas, colunas = configuracoes()
 criarTabuleiro(linhas, colunas)
 verificarJogada()
+
